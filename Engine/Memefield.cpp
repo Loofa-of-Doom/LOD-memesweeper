9f33c43fb8abfb352facc021199bf4a0831746b1
Loofa of Doom <abe2fresh@gmail.com>
2023-10-29 11:31:33 -0400

    made constructor and maybe some other stuff cant rmemver

--- Engine/Memefield.cpp
+++ Engine/Memefield.cpp
@@ -1,1 +1,18 @@
 #include "Memefield.h"
+#include <random>
+
+void Memefield::Tile::SpawnMemes()
+{
+}
+
+Memefield::Memefield(int in_nMemes)
+	:
+	nMemes(in_nMemes),
+	background(0, width * SpriteCodex::tileSize, 0, height * SpriteCodex::tileSize)
+{
+}
+
+Memefield::Tile& Memefield::AtTile(Vei2& tileLocation)
+{
+	return tiles[tileLocation.y * width + tileLocation.x];
+}
--- Engine/Memefield.h
+++ Engine/Memefield.h
@@ -1,4 +1,9 @@
 #pragma once
+#include "Vei2.h"
+#include "Graphics.h"
+#include "RectI.h"
+#include "SpriteCodex.h"
+
 class Memefield
 {
 public:
@@ -12,12 +17,20 @@ private:
 	class Tile
 	{
 	public:
+		void SpawnMemes();
 	private:
 		State state = State::Hidden;
+		bool hasMeme = false;
 	};
 public:
+	Memefield(int in_nMemes);
+	Tile& AtTile(Vei2& tileLocation);
 private:
 	static constexpr int width = 20;
 	static constexpr int height = 15;
+	int nMemes;
+	Tile tiles[width * height];
+	RectI background;
+	
 };
 
